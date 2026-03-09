36291c0242e1c0dfe420aa1f82d45ebe014e38b9
matsumoto <matzz0219@gmail.com>
2026-03-10 00:49:26 +0900

    スケジュール新規作成画面を作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,47 @@
 class SchedulesController < ApplicationController
+  def index
+    @schedules = Schedule.all
+  end
+
+  def show
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to schedules_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def update
+    @schedule = Schedule.find(params[:id])
+    if @schedule.update(schedule_params)
+      redirect_to schedules_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @schedule = Schedule.find(params[:id])
+    @schedule.destroy
+    redirect_to schedules_path
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:title, :start_date, :end_date, :all_day, :memo)
+  end
 end
